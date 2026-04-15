5c766fabdb418fd3ae2605ab397f930fb0e4fa60
Mykhailo Yukhman <mykhailo.yukhman.25@kse.org.ua>
2026-04-15 20:39:39 +0300

    created a json file with filtered data

--- task1.py
+++ task1.py
@@ -9,3 +9,9 @@ df["distance"] = (df["x"] **2 + df["y"] ** 2)**(1/2)
 max = df["distance"].max()
 avrg = df["distance"].mean()
 
+print(f"Max distance is {max}, average distance is {avrg}")
+
+more_than_avrg = df[df["distance"] > avrg]
+print(f"Filtered distance: {more_than_avrg}")
+
+more_than_avrg.to_json("filtered_walk.json", orient="records")
